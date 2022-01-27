b2633803d9aaac240efb79b7a53357f57f39e838
Redwan <rdwnsjjd@gmail.com>
2022-01-27 11:49:16 +0330

    refining code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Clocou
 Your working clock calculator!
+
+
+## How to run?
+use `python3 src/main.py`
---
+++ common/module.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2022 Redwan
+# 
+# This file is part of clocou.
+# 
+# clocou is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# clocou is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with clocou.  If not, see <http://www.gnu.org/licenses/>.
+
+def get_banner():
+    return "      .g8""'bgd   '7MM                                             \n"\
+"    .dP'     `M   MM                                             \n"\
+"    dM'       `   MM   ,pW'Wq.    ~p6'bo   ,pW'Wq.  '7MM  `7MM   \n"\
+"    MM            MM  6W'   `Wb  6M'  OO  6W'   `Wb   MM    MM   \n"\
+"    MM.           MM  8M     M8  8M       8M     M8   MM    MM   \n"\
+"    `Mb.     ,'   MM  YA.   ,A9  YM.    , YA.   ,A9   MM    MM   \n"\
+"      `'bmmmd'  .JMML. `Ybmd9'    YMbmd'   `Ybmd9'    `Mbod'YML. \n"
